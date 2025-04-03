7c53265f7d47c4079942c502cb7d6e197aa0a9b6
rohit <singhrohit0047@gmail.com>
2025-04-03 04:18:07 -0700

    updated tfstate

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source ="hashicorp/azurerm"
+            version = "4.24.0"
+        }
+    }
+}
+
+
+provider "azurerm" {
+    features {}
+    subscription_id = "1a65c9a7-b8cc-4be5-8bb1-997cca837c32"
+}
+
+resource "azurerm_resource_group" "rgrg01" {
+    name = "stst01"
+    location ="Central India"
+}
